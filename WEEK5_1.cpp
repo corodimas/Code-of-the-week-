e3e39a6c76b96a0eaf897be0d6326365b2ad3e70
corodimas <88823702+corodimas@users.noreply.github.com>
2021-09-15 18:40:33 +0700

    Add files via upload

---
+++ WEEK5_1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int factorial(int x)
+{
+    if(x==0)
+       return 1;
+    else
+       return x * factorial(x-1);
+}
+
+int main()
+{
+    int number;
+    scanf("%ld",&number);
+    printf("%lld",factorial(number));
+}
